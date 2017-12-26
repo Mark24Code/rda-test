ca88c428de82c1607874ca608e769374d58ed910
Mark24 <mark.zhangyoung@qq.com>
2017-12-26 17:45:33 +0800

    table

--- src/router.js
+++ src/router.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 
+import Products from "./routes/Products.js";
+
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/Products" component={Products} />
       </Switch>
     </Router>
   );
