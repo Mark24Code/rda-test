e62994140fadf4caf19d91e9093fcc735979db8a
Mark24 <mark.zhangyoung@qq.com>
2017-12-28 15:39:25 +0800

    add user

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = dva({
 app.use(createLoading());
 
 // 3. Model
-app.model(require('./models/products'));
+app.model(require('./models/user'));
 
 // 4. Router
 app.router(require('./router'));
--- src/router.js
+++ src/router.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 
-import Products from "./routes/Products.js";
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
-        <Route path="/Products" component={Products} />
       </Switch>
     </Router>
   );
